8e1cf973a2da4bd22f93e8407e0cea2149210910
geragt280 <ben.ten550@gmail.com>
2024-03-21 20:24:52 +0500

    footer almost completed

---
+++ assets/Strings/NavigationString.ts
@@ -0,0 +1,52 @@
+export const NavigationRows = [
+    "LogoDetails",
+    "OurCompany",
+    "QuickLinks",
+    "NewsLetter"
+]
+
+export const ourCompanyLinks = [
+    {
+        name: "Our Story",
+        link: "#"
+    },
+    {
+        name: "Travel Blog & Tips",
+        link: "#"
+    },
+    {
+        name: "Working With Us",
+        link: "#"
+    },
+    {
+        name: "Be Our Partnet",
+        link: "#"
+    },
+    {
+        name: "Support Policy",
+        link: "#"
+    }
+];
+
+export const quickLinks = [
+    {
+        name: "View Account",
+        link: "#"
+    },
+    {
+        name: "Track Your Order",
+        link: "#"
+    },
+    {
+        name: "Suggestions",
+        link: "#"
+    },
+    {
+        name: "Blog & Update",
+        link: "#"
+    },
+    {
+        name: "Newsletters",
+        link: "#"
+    }
+];
--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -1,48 +1,44 @@
-"use client"
-import Image from 'next/image';
-import { useState } from 'react';
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 import mobile_download from "../../assets/footer/mobile_app_download.png";
 import logo from "../../assets/common/Logo.png";
 import sc0 from "../../assets/footer/01.svg";
-import sc1 from "../../assets/footer/01.svg";
-import sc2 from "../../assets/footer/02.svg";
-import sc3 from "../../assets/footer/03.svg";
+import sc1 from "../../assets/footer/02.svg";
+import sc2 from "../../assets/footer/03.svg";
+import sc3 from "../../assets/footer/04.svg";
+import { NavigationRows } from "../../assets/Strings/NavigationString";
+import LogoDetails from "./LogoDetails";
+import OurCompany from "./OurCompany";
+import QuickLinks from "./QuickLinks";
+import OurNewsLetter from "./OurNewsLetter";
 
 const Footer = () => {
-
-    return (
-        <div className='text-black bg-white relative items-center flex flex-col'>
-            <div className='flex items-center justify-center flex-col relative pt-16 pb-16'>
-                <Image src={mobile_download} alt='not_found_image' className='w-[90%]' />
-            </div>
-            <div className='h-96 w-[90%]'>
-                <div>
-                    <div><Image src={logo} alt='not_found_image' /></div>
-                    <div>
-                        <p>
-                            A new way to make the payments easy, reliable and 100% secure. claritatem itamconse quat. Exerci tation ullamcorper.
-                        </p>
-                    </div>
-                    <div className='flex w-72 justify-between'>
-                        <Image src={sc0} alt='not_found_image' width={50} height={50} />
-                        <Image src={sc1} alt='not_found_image' width={50} height={50} />
-                        <Image src={sc2} alt='not_found_image' width={50} height={50} />
-                        <Image src={sc3} alt='not_found_image' width={50} height={50} />
-                    </div>
-                </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-            </div>
-            I am bottom navigation
-        </div>
-    );
+  return (
+    <div className="text-black bg-white relative items-center flex flex-col">
+      <div className="flex items-center justify-center flex-col relative pt-16 pb-16">
+        <Image
+          src={mobile_download}
+          alt="not_found_image"
+          className="w-[90%]"
+        />
+      </div>
+      <div className="lg:h-96 w-[90%] mb-5 flex flex-col lg:flex-row justify-between">
+        {NavigationRows.map((item, i) => {
+          if (item === "LogoDetails") {
+            return <LogoDetails />
+          } else if (item === "OurCompany") {
+            return <OurCompany />
+          } else if (item === "QuickLinks") {
+            return <QuickLinks />
+          } else if (item === "NewsLetter") {
+            return <OurNewsLetter />
+          }
+          return <div></div>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

8b31b821e0d3c552ed899607632cd09594513300
salman <alysalman15@gmail.com>
2024-03-21 19:40:40 +0500

    sidebar complete

--- app/pages/customerreview/page.tsx
+++ app/pages/customerreview/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Background_img from "../../../assets/customer_review/Rectangle 19478.png";
 import Information_Icon from "../../../assets/customer_review/Vector.png";
@@ -10,10 +11,9 @@ import ProgressBar from "./progressBar";
 import RatingProgressBar from "./ratingProgressBar";
 import Rating from "./rating";
 
-import MyCalender from "./datePicker";
+import Ticket_Dropdown from "./dropdown";
 
 export default function CustomerReview() {
-  console.log(":hehe rae");
   return (
     <div className="mb-10 bg-white">
       <div className="relative">
@@ -42,7 +42,7 @@ export default function CustomerReview() {
         </div>
       </div>
       <div className="flex justify-center gap-9 mt-10">
-        <div className="w-[60%] px-8 border-2 rounded-md py-10">
+        <div className="w-[54%] px-8 border-2 rounded-md py-10">
           <h2 className="text-xl font-bold mt-4 mb-4 border-l-2 border-l-[#1677FF] px-1">
             Customer Review
           </h2>
@@ -89,13 +89,72 @@ export default function CustomerReview() {
           </div>
         </div>
 
-        <div className="w-[20%] bg-[#F8F8F8] px-8 py-12">
+        <div className="w-[26%] bg-[#F8F8F8] px-8 py-12">
           <h2 className="text-lg font-bold mt-4 mb-4 border-l-2 border-l-[#1677FF] px-1">
             Book This Tour
           </h2>
           <div>
-            <MyCalender />
+            <input className="w-full" type="datetime-local" name="" id="" />
           </div>
+          <div className="w-full flex gap-4 mt-4">
+            <h2 className=" font-semibold">Time:</h2>
+            <input className="w-full" type="time" name="" id="" />
+          </div>
+          <div className="w-full mt-4">
+            <h2 className="font-semibold">Tickets:</h2>
+            <div className="flex items-center justify-between my-2">
+              <p className="w-full text-[13px] text-gray-400">
+                Children(0-12 years)$129
+              </p>
+              <Ticket_Dropdown />
+            </div>
+            <div className="flex items-center justify-between my-2">
+              <p className="w-full text-[13px] text-gray-400">
+                Youth(13-17 years)$169
+              </p>
+              <Ticket_Dropdown />
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="w-full text-[13px] text-gray-400">
+                Adult (18+ years)$189.00
+              </p>
+              <Ticket_Dropdown />
+            </div>
+          </div>
+          <div className="w-full gap-4 mt-4">
+            <h2 className=" font-semibold">Add Extra:</h2>
+            <div className="flex gap-x-2 mt-2">
+              <input type="checkbox" name="" id="" />
+              <p className="text-gray-400">Service per booking</p>
+            </div>
+            <div>
+              <div className="mt-2">
+                <div className="flex gap-x-2">
+                  <input type="checkbox" name="" id="" />
+                  <p className="text-gray-400">Service per booking</p>
+                </div>
+                <div className="ml-5">
+                  <div className="flex gap-x-6">
+                    <p className="text-gray-400">Adult:</p>
+                    <h3>$18.00</h3>
+                  </div>
+                  <div className="flex gap-x-6">
+                    <p className="text-gray-400">Adult:</p>
+                    <h3>$18.00</h3>
+                  </div>
+                  <div className="flex gap-x-6">
+                    <p className="text-gray-400">Adult:</p>
+                    <h3>$18.00</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between mt-4 mb-6">
+            <h2 className=" font-semibold">Total:</h2>
+            <h3 className="text-[#1677FF] font-bold">$130.00</h3>
+          </div>
+          <button className="w-11/12 py-3 flex justify-center m-auto rounded-sm text-white bg-[#1677FF]">Procced Booking</button>
         </div>
       </div>
     </div>

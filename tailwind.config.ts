13d79c1b9f113f6881bd0b6bf04b803ef00cc785
Rajja Farhan <rajjafarhan2@gmail.com>
2024-03-21 23:51:16 +0500

    rajja

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
@@ -21,6 +24,8 @@ const config: Config = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 };
 export default config;

1b3606de6ffc791195d4416c9cf0e95b9dc7c8fb
geragt280 <ben.ten550@gmail.com>
2024-03-22 02:52:20 +0500

    Mobile responsive for mail box fixed, Affiliations added in footer

--- assets/Strings/NavigationString.ts
+++ assets/Strings/NavigationString.ts
@@ -1,3 +1,8 @@
+import fbr_logo from "../../assets/common/fbr-logo.svg";
+import govt_pk from "../../assets/common/govt_pk-logo.svg";
+import secp_logo from "../../assets/common/secp-logo.svg";
+import taap_logo from "../../assets/common/taap-logo.svg";
+
 export const NavigationRows = [
     "LogoDetails",
     "OurCompany",
@@ -50,3 +55,22 @@ export const quickLinks = [
         link: "#"
     }
 ];
+
+export const AffiliationsData = [
+    {
+      name: "FBR",
+      image: fbr_logo
+    },
+    {
+      name: "Govt-Pvt",
+      image: govt_pk
+    },
+    {
+      name: "SECP",
+      image: secp_logo
+    },
+    {
+      name: "TAAP",
+      image: taap_logo
+    },
+  ];
--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
 import mobile_download from "../../assets/footer/mobile_app_download.png";
-import logo from "../../assets/common/Logo.png";
-import sc0 from "../../assets/footer/01.svg";
-import sc1 from "../../assets/footer/02.svg";
-import sc2 from "../../assets/footer/03.svg";
-import sc3 from "../../assets/footer/04.svg";
 import { NavigationRows } from "../../assets/Strings/NavigationString";
 import LogoDetails from "./LogoDetails";
 import OurCompany from "./OurCompany";
 import QuickLinks from "./QuickLinks";
 import OurNewsLetter from "./OurNewsLetter";
+import Affiliations from "./Affiliations";
 
 const Footer = () => {
   return (
-    <div className="text-black bg-white relative items-center flex flex-col">
-      <div className="flex items-center justify-center flex-col relative pt-16 pb-16">
+    <div className="text-black bg-white relative pb-4 pt-8 items-center flex flex-col">
+      <Affiliations />
+      <div className="flex items-center justify-center flex-col relative pt-20 pb-16">
         <Image
           src={mobile_download}
           alt="not_found_image"

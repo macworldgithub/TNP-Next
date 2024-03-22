0cee74317ad53e69d784cc0434519de279803fc3
Rajja Farhan <rajjafarhan2@gmail.com>
2024-03-22 17:40:48 +0500

    .

--- app/pages/domestic/page.tsx
+++ app/pages/domestic/page.tsx
@@ -29,15 +29,14 @@ const Domestic = () => {
 involve travel by foot, bicycle, automobile, train, boat, bus, airplane, or other means, with or 
 without luggage, and can be one way or  round trip. Travel can also include relatively short stays 
 between successive movements.
-
 The origin of the word “travel” is most likely lost to history. The term “travel” may  originate from 
 the Old French word travail, which means ‘work’. According to the  Merriam Webster dictionary, 
 the first known use of the word travel was in the 14th century."/>
 
 
+                {/* aqsa add your compoennts here */}
                 </div>
 
-{/* aqsa add your compoennts here */}
 
 
                 {/* left side */}

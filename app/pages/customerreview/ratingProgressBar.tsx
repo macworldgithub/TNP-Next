a5c5ac3326c3c16d975b4933d2e4b3677e581bff
salman <alysalman15@gmail.com>
2024-03-23 00:31:38 +0500

    customer_page complete (still not responsive)

--- app/pages/customerreview/ratingProgressBar.tsx
+++ app/pages/customerreview/ratingProgressBar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Flex, Progress } from "antd";
+import "./style.css"
 
 const RatingProgressBar: React.FC = () => (
-  <Flex gap="small" vertical>
-    <Progress percent={60} />
+  <Flex gap="small" vertical style={{color:"#00ADEE"}}>
+    <Progress percent={60} style={{ color: "#FBAD17" }} />
   </Flex>
 );
 

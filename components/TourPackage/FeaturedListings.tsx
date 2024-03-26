83e9f88290d2b80f19b04d8d14427f73a56b4a3e
geragt280 <ben.ten550@gmail.com>
2024-03-26 07:55:49 +0500

    Fixed some issues in package details(domestic) screen

--- components/TourPackage/FeaturedListings.tsx
+++ components/TourPackage/FeaturedListings.tsx
@@ -9,8 +9,8 @@ import TourCard from "./TourCard";
 import Dropdown from "../Dropdown";
 const FeaturedListings = () => {
   const [currPage, setCurrPage] = useState(1);
-  const itemsPerPage = 6; 
-  
+  const itemsPerPage = 8;
+
   const featuredData = [
     {
       img: img1,
@@ -73,138 +73,147 @@ const FeaturedListings = () => {
       reviewCount: 1,
     },
     {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
-      {
-        img: img2,
-        location: "Pakistan-North",
-        title: "All Girls Trip to Kashmir ",
-        duration: "7days",
-        people: 12,
-        price: 32000,
-        discountedPrice: 20987,
-        reviewCount: 1,
-      },
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
+    {
+      img: img2,
+      location: "Pakistan-North",
+      title: "All Girls Trip to Kashmir ",
+      duration: "7days",
+      people: 12,
+      price: 32000,
+      discountedPrice: 20987,
+      reviewCount: 1,
+    },
   ];
 
-  const filterOptions =[
-    {label:"All Filters" , value:"All filters"},
-    {label:"All Filters" , value:"All filters"},
-    {label:"All Filters" , value:"All filters"}
-  ]
-  const dateOptions =[
-    {label:"Dates" , value:"dates"},
-    {label:"Dates" , value:"dates"},
-    {label:"Dates" , value:"dates"}
-  ]
-  const typeOptions =[
-    {label:"Types" , value:"types"},
-    {label:"Types" , value:"types"},
-    {label:"Types" , value:"types"}
-  ]
-  const priceOptions =[
-    {label:"Price" , value:"price"},
-    {label:"Price" , value:"price"},
-    {label:"Price" , value:"price"}
-  ]
-  const [selectedFilter , setSelectedFilter] = useState(filterOptions[0].value);
-  const [selectedDates , setSelectedDates] = useState(dateOptions[0].value);
-  const [selectedType , setSelectedType] = useState(typeOptions[0].value);
-  const [ selectedPrice , setSelectedPrice] = useState(priceOptions[0].value);
+  const filterOptions = [
+    { label: "All Filters", value: "All filters" },
+    { label: "All Filters", value: "All filters" },
+    { label: "All Filters", value: "All filters" },
+  ];
+  const dateOptions = [
+    { label: "Dates", value: "dates" },
+    { label: "Dates", value: "dates" },
+    { label: "Dates", value: "dates" },
+  ];
+  const typeOptions = [
+    { label: "Types", value: "types" },
+    { label: "Types", value: "types" },
+    { label: "Types", value: "types" },
+  ];
+  const priceOptions = [
+    { label: "Price", value: "price" },
+    { label: "Price", value: "price" },
+    { label: "Price", value: "price" },
+  ];
+  const [selectedFilter, setSelectedFilter] = useState(filterOptions[0].value);
+  const [selectedDates, setSelectedDates] = useState(dateOptions[0].value);
+  const [selectedType, setSelectedType] = useState(typeOptions[0].value);
+  const [selectedPrice, setSelectedPrice] = useState(priceOptions[0].value);
 
   const startIndex = (currPage - 1) * itemsPerPage;
   const endIndex = Math.min(startIndex + itemsPerPage, featuredData.length);
   const currentItems = featuredData.slice(startIndex, endIndex);
 
-  
-
-
- 
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="w-[80%] flex flex-col gap-4  ">
-        <PaginationInfo currentPage={2} itemsPerPage={6} totalItems={featuredData.length} />
+        <PaginationInfo
+          currentPage={2}
+          itemsPerPage={6}
+          totalItems={featuredData.length}
+        />
         <div className="flex flex-row gap-4 flex-wrap ">
-          
-                <Dropdown options={filterOptions} selectedOption={selectedFilter} onSelect={setSelectedFilter}/>
-                <Dropdown options={dateOptions} selectedOption={selectedDates} onSelect={setSelectedDates}/>
-                <Dropdown options={typeOptions} selectedOption={selectedType} onSelect={setSelectedType}/>
-                <Dropdown options={priceOptions} selectedOption={selectedPrice} onSelect={setSelectedFilter}/>
-
-
-
-         
-        
-     
+          <Dropdown
+            options={filterOptions}
+            selectedOption={selectedFilter}
+            onSelect={setSelectedFilter}
+          />
+          <Dropdown
+            options={dateOptions}
+            selectedOption={selectedDates}
+            onSelect={setSelectedDates}
+          />
+          <Dropdown
+            options={typeOptions}
+            selectedOption={selectedType}
+            onSelect={setSelectedType}
+          />
+          <Dropdown
+            options={priceOptions}
+            selectedOption={selectedPrice}
+            onSelect={setSelectedFilter}
+          />
         </div>
-        <div className="w-full flex flex-row   flex-wrap gap-10">
+        <div className="w-full flex flex-row flex-wrap gap-10">
           {currentItems.map((item, index) => (
             <TourCard
               pic={item?.img}
--- components/TourPackage/TourPackHero.tsx
+++ components/TourPackage/TourPackHero.tsx
@@ -3,25 +3,25 @@ import { url } from "inspector";
 import bg from "../../public/TourPackage/heroimg.png";
 import Image from "next/image";
 import TourpackSearch from "./TourPackSearch";
-const TourPackHero = ()=>{
-    return(
-        <div className="w-full xl:h-[70vh] lg:h-[80vh] md:h-[100vh] h-[140vh] " >
-            <div className=" w-full  h-[60%]  relative">
-            <div className="w-full h-full bg-gradient-to-r from-slate-800 to-slate-100   absolute z-10 opacity-70">
-                <p className="text-4xl font-bold text-gray-900 text-center mt-16">Domestic</p>
-            </div>
-            <div className="absolute inset-0 z-0">
-                <Image src={bg} alt="background image" layout="fill"  />
-            </div>
-            <div className="absolute w-full z-20 xl:top-[83%] lg:top-[70%] md:top-[66%] top-[65%] ">
-                <div className="w-full flex justify-center">
-                <TourpackSearch/>
-                </div>
-                </div>
-            </div>
-            
-
+const TourPackHero = () => {
+  return (
+    <div className="w-full xl:h-[60vh] lg:h-[50vh] md:h-[40vh] h-[40vh] ">
+      <div className=" w-full h-[60%]  relative">
+        <div className="w-full h-full bg-gradient-to-r from-slate-800 to-slate-100   absolute z-10 opacity-70">
+          <p className="text-4xl font-bold text-gray-900 text-center mt-16">
+            Domestic
+          </p>
         </div>
-    )
-}
+        <div className="absolute inset-0 z-0">
+          <Image src={bg} alt="background image" layout="fill" />
+        </div>
+        <div className="absolute w-full z-20 xl:top-[83%] lg:top-[70%] md:top-[66%] top-[65%] ">
+          <div className="w-full flex justify-center">
+            <TourpackSearch />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default TourPackHero;

a3de45a5f2c813fd7dd9bc92283879e8480d9434
usman-90 <siddiquiusman328@gmail.com>
2024-04-18 13:37:11 +0500

    endpoints fixed

--- app/pages/api/user/route.ts
+++ app/pages/api/user/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: Request) {
   try {
     // Your logic here
     // ...
-    return new NextResponse('Success', { status: 200 });
+    return NextResponse.json({ status: 200, message: "Complete" });
   } catch (error) {
     console.error('Error in GET handler:', error);
     return new NextResponse('Internal Server Error', { status: 500 });

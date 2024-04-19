b77e3e3967e2dd59f086e620be87f7d391a2e040
usman-90 <siddiquiusman328@gmail.com>
2024-04-19 18:07:43 +0500

    prisma setup complete

--- app/pages/api/tourpackages/route.ts
+++ app/pages/api/tourpackages/route.ts
@@ -4,7 +4,8 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const prisma = new PrismaClient();
   try {
-    const packages = prisma.tnp_packages.findMany();
+    const packages = await prisma.tnp_packages.findMany();
+    console.log("Packages", packages);
     return NextResponse.json({ status: 200, message: "Success", data: packages });
   } catch (error) {
     console.error('Error in GET handler:', error);
@@ -16,6 +17,16 @@ export async function POST(request: Request) {
   try {
     // Your logic here
     // ...
+    return new NextResponse("Success", { status: 200 });
+  } catch (error) {
+    console.error('Error in POST handler:', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
+export async function PUT(request: Request) {
+  try {
+    //update logic here
     return new NextResponse("Success", { status: 200 });
   } catch (error) {
     console.error('Error in POST handler:', error);

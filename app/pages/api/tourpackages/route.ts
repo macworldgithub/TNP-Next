e4e6fa4f110197e95f031c45c559404f97e04b90
usman-90 <siddiquiusman328@gmail.com>
2024-04-19 19:19:09 +0500

    api work structure for packages

--- app/pages/api/tourpackages/route.ts
+++ app/pages/api/tourpackages/route.ts
@@ -1,6 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
+interface package_structure {
+  package_name: string;
+  package_category_id: number;
+  package_type_id: number;
+  package_region_id: number;
+  package_total_persons: number;
+  package_description: string;
+  package_rate_normal: number;
+  package_rate_deluxe: number;
+  package_details: string;
+}
+
 export async function GET(request: Request) {
   const prisma = new PrismaClient();
   try {
@@ -13,11 +25,16 @@ export async function GET(request: Request) {
   }
 }
 
-export async function POST(request: Request) {
+export async function POST(request: package_structure) {
+  const prisma = new PrismaClient();
   try {
     // Your logic here
     // ...
-    return new NextResponse("Success", { status: 200 });
+
+    // const insert = await prisma.tnp_packages.create({ data: { package_name: request.body.} })
+    // const response = await request.json();
+    // console.log("Body", response);
+    return NextResponse.json({ status: 200, message: "Success", data: [] });
   } catch (error) {
     console.error('Error in POST handler:', error);
     return new NextResponse('Internal Server Error', { status: 500 });

bec6a273733c7be749fb03c1ed570aeb47c958ff
salman <alysalman15@gmail.com>
2024-04-26 13:06:56 +0500

    honeymoon screen complete

--- components/Domestics/Cost.tsx
+++ components/Domestics/Cost.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import check from "../../public/domestic/check.png";
-import cancel from "../../public/domestic/Cancel.png"
+import check from "../../public/domestic/check.svg";
+import cancel from "../../public/domestic/cancel.svg"
 const Cost = ({includeCost,costExclude}:any) => {
   
 
@@ -9,8 +9,8 @@ const Cost = ({includeCost,costExclude}:any) => {
     <div className="flex flex-col gap-6 mt-4 p-6 lg:p-0 ">
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-4">
-          <p className="text-2xl">The Cost Includes</p>
-          <div className="flex flex-col gap-2">
+          <p className="text-2xl text-white">The Cost Includes</p>
+          <div className="flex flex-col text-white gap-2">
             {includeCost.map((item : any, index :any) => (
               <div key={index} className="flex flex-row gap-4">
                 <Image
@@ -23,8 +23,8 @@ const Cost = ({includeCost,costExclude}:any) => {
             ))}
           </div>
           <div className="flex flex-col gap-4">
-            <p className="text-2xl ">The Cost Excludes</p>
-            <div className="flex flex-col gap-2">
+            <p className="text-2xl text-white ">The Cost Excludes</p>
+            <div className="flex flex-col text-white gap-2">
                 {costExclude.map((item : any, index : any)=>(
                     <div key={index} className="flex flex-row gap-4">
                         <Image src={cancel} alt="cancel icon.." className="h-4 w-4 mt-[4px]"/>

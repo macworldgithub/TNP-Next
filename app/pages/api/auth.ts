261ee6bffdd6029d03f241a27dbfb288095dc963
usman-90 <siddiquiusman328@gmail.com>
2024-04-29 11:06:08 +0500

    code updated on auth class

--- app/pages/api/auth.ts
+++ app/pages/api/auth.ts
@@ -2,48 +2,44 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import { NextRequest } from 'next/server'
 
-export const comparePasswords = (password : string, hash : string) => {
-  return bcrypt.compare(password, hash)
+export const comparePasswords = (password: string, hash: string) => {
+  return bcrypt.compare(password, hash);
 }
 
-export const hashPassword = (password : string) => {
-  return bcrypt.hash(password, 5)
+export const hashPassword = (password: string) => {
+  return bcrypt.hash(password, 5);
 }
 
-export const createJWT = (user : any) => {
+export const createJWT = (user: any) => {
   const token = jwt.sign({
-      id: user.id,
-      email: user.email
-    }, 
+    id: user.id,
+    email: user.email
+  },
     process.env.JWT_SECRET
-  )
-  return token
+  );
+  return token;
 }
 
-export const protect = (req : NextRequest) => {
+export const protect = (req: NextRequest) => {
 
-  const bearer = req.headers.get("authorization")
+  const bearer = req.headers.get("authorization");
 
   if (!bearer) {
-    return false
+    return false;
   }
 
-  const [, token] = bearer.split(' ')
+  const [_, token] = bearer.split(' ');
 
   if (!token) {
-    return false
+    return false;
   }
 
   try {
-    const user = jwt.verify(token, process.env.JWT_SECRET)
-    req.headers.set("user",JSON.stringify(user))
-    return true
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.headers.set("user", JSON.stringify(user));
+    return true;
   } catch (e) {
-    console.error(e)
-    return false
+    console.error(e);
+    return false;
   }
 }
-
-
-
-

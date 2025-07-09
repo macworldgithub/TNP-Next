796adad4d6c8147d76bd1dfccf6abda52007c3b8
HUZAIFA <karimjawwad09@gmail.com>
2025-07-09 13:01:43 +0500

    Dynamic URL issue fixed

--- app/pages/api/admin/getBanners/single/route.ts
+++ app/pages/api/admin/getBanners/single/route.ts
@@ -1,6 +1,49 @@
+// import { NextRequest, NextResponse } from "next/server";
+// import prisma from "../../../db";
+// import { createJWT } from "../../../auth";
+
+// export async function GET(request: NextRequest) {
+//   try {
+//     const searchParams = request.nextUrl.searchParams;
+//     const pagename = searchParams.get("pagename");
+//     console.log(pagename);
+//     if (!pagename) {
+//       return NextResponse.json({
+//         status: 400,
+//         message: "send page in query param",
+//       });
+//     }
+//     const banner = await prisma.tnp_banner.findMany({
+//       where: {
+//         tnp_banner_page: pagename,
+//       },
+//     });
+
+//     // const token = createJWT(banner);
+
+//     // let userData = {
+//     //   token,
+//     //   name: user?.name,
+//     //   lname: user?.lname,
+//     //   email: user?.email,
+//     //   id: user?.id,
+//     // };
+//     return NextResponse.json({
+//       status: 200,
+//       message: "success",
+//       data: banner,
+//     });
+//   } catch (error) {
+//     console.error("Error in GET handler:", error);
+//     return new NextResponse("Internal Server Error", { status: 500 });
+//   }
+// }
+
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../db";
 import { createJWT } from "../../../auth";
+
+export const dynamic = 'force-dynamic'; // Add this line to force dynamic rendering
 
 export async function GET(request: NextRequest) {
   try {

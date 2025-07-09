f8d3d5dd9f8fc594f57f3d833befa05601b6abe6
shweeta <shweeta@macworldtechnology.com>
2025-07-09 11:34:08 +0500

    update

--- components/Login/loginForm.tsx
+++ components/Login/loginForm.tsx
@@ -60,12 +60,12 @@ const LoginForm = () => {
                 console.log("User",user)
                
             }).catch((error) => {
-                // Handle Errors here.
+           
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                // The email of the user's account used.
+               
                 const email = error.customData.email;
-                // The AuthCredential type that was used.
+               
                 const credential = GoogleAuthProvider.credentialFromError(error);
                 // ...
             });
